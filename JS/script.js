1ce72f481925068332e6270beb7570d4bb56120b
Jasmine Poindexter <poindexterjasmine@gmail.com>
2021-02-25 20:53:23 -0600

    Final Draft

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,14 @@
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("question");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
+        this.classList.toggle("active");
+    var faq = this.nextElementSibling;
+    if (faq.style.display === "block") {
+      faq.style.display = "none";
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      faq.style.display = "block";
     }
   });
 }
